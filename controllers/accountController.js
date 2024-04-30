e3fa6be8b28757a34b5d9709dabceac2d24e0b47
venkatthadi <venkat@mobicip.com>
2024-04-30 11:37:43 +0530

    added model

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,30 +1,30 @@
-import { getAccounts, getAccount, createAccount, updateAccount, deleteAccount } from '../database.js';
+import { getAccounts, getAccount, createAccount, updateAccount, deleteAccount } from '../models/accountModel.js';
 
 export const getAccs = async (req, res) => {
     const accs = await getAccounts()
-    res.send(accs)
+    res.status(200).send(accs)
 }
 
 export const getAcc = async (req, res) => {
     const acc = await getAccount(req.params.id)
-    res.send(acc)
+    res.status(200).send(acc)
 }
 
 export const createAcc = async (req, res) => {
     const { name } = req.body
     const account = await createAccount(name)
-    res.send(account)
+    res.status(201).send(account)
 }
 
 export const updateAcc = async (req, res) => {
     const id = req.params.id
     const { name } = req.body
     const account = await updateAccount(id, name)
-    res.send(account)
+    res.status(200).send(account)
 }
 
 export const deleteAcc = async (req, res) => {
     const id = req.params.id
     await deleteAccount(id)
-    res.send({ message: "Account deleted successfully" })
+    res.status(204).send({ message: "Account deleted successfully" })
 }
